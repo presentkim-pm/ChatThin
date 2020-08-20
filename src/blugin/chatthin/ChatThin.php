c739170c8388bf6a1a290f62c658cc6778edb647
PresentKim <debe3721@gmail.com>
2020-08-20 17:54:41 +0900

    Fixed all errors after api changed to 3.0.0

--- src/blugin/chatthin/ChatThin.php
+++ src/blugin/chatthin/ChatThin.php
@@ -46,14 +46,13 @@ class ChatThin extends PluginBase implements Listener{
      * @param DataPacketSendEvent $event
      */
     public function onDataPacketSendEvent(DataPacketSendEvent $event) : void{
-        foreach($event->getPackets() as $_ => $pk){
-            if(!$pk instanceof TextPacket || $pk->type === TextPacket::TYPE_TIP || $pk->type === TextPacket::TYPE_POPUP || $pk->type === TextPacket::TYPE_JUKEBOX_POPUP)
-                continue;
+        $pk = $event->getPacket();
+        if(!$pk instanceof TextPacket || $pk->type === TextPacket::TYPE_TIP || $pk->type === TextPacket::TYPE_POPUP || $pk->type === TextPacket::TYPE_JUKEBOX_POPUP)
+            return;
 
-            if($pk->type === TextPacket::TYPE_TRANSLATION){
-                $pk->message = preg_replace("/%*(([a-z0-9_]+\.)+[a-z0-9_]+)/i", "%$1", $pk->message);
-            }
-            $pk->message .= self::THIN_TAG;
+        if($pk->type === TextPacket::TYPE_TRANSLATION){
+            $pk->message = preg_replace("/%*(([a-z0-9_]+\.)+[a-z0-9_]+)/i", "%$1", $pk->message);
         }
+        $pk->message .= self::THIN_TAG;
     }
 }
