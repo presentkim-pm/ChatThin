3f015f1ae902799231e242ea641f324e95c292cb
PresentKim <debe3721@gmail.com>
2020-08-20 17:53:02 +0900

    Remove dirty trick (support both api 3.0.0 and 4.0.0)

--- src/blugin/chatthin/ChatThin.php
+++ src/blugin/chatthin/ChatThin.php
@@ -46,8 +46,7 @@ class ChatThin extends PluginBase implements Listener{
      * @param DataPacketSendEvent $event
      */
     public function onDataPacketSendEvent(DataPacketSendEvent $event) : void{
-        $packets = method_exists($event, 'getPacket') ? [$event->getPacket()] : $event->getPackets();
-        foreach($packets as $_ => $pk){
+        foreach($event->getPackets() as $_ => $pk){
             if(!$pk instanceof TextPacket || $pk->type === TextPacket::TYPE_TIP || $pk->type === TextPacket::TYPE_POPUP || $pk->type === TextPacket::TYPE_JUKEBOX_POPUP)
                 continue;
 
